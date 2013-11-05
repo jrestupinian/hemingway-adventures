f33bc1626258aa1ba49041d3168b30fe90af0189
OpenShift guest <jrestupinian@gmail.com>
2013-11-04 19:40:48 -0500

    Rails new command

---
+++ hemingway-adventures/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HemingwayAdventures::Application.config.secret_token = 'bdd3cb02b46369228f5ecc5c9ac09c903b9ac743b89b8b9dfd888883ad108740940947a785c41ae0fd766398737e4f2681987370a4431e8a37875c65d8e4f3f8'
