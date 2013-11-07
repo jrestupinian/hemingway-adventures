4ba1c0114c0b9a359947f8ee04711c0b250ddd9f
OpenShift guest <jrestupinian@gmail.com>
2013-11-06 23:19:00 -0500

    proyecto regenerated

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HemingwayAdventures::Application.config.secret_token = 'bdd3cb02b46369228f5ecc5c9ac09c903b9ac743b89b8b9dfd888883ad108740940947a785c41ae0fd766398737e4f2681987370a4431e8a37875c65d8e4f3f8'
+HemingwayAdventures::Application.config.secret_token = 'f0d709ab509e51386d8498b7534fe122027f084bae9e817afe23cef58c7dfa19927ce2213be458956d1b8d641b926885d06a0abb4685900d9a69e86384722606'
